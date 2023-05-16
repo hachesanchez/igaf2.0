ffd2a771568027ed47a49190fff01ae3e5c23a33
CristianMausque <cristianmausque@gmail.com>
2023-05-16 11:05:39 +0200

    cristian changes

--- public/js/dynamic-ingredient-form.js
+++ public/js/dynamic-ingredient-form.js
@@ -2,9 +2,9 @@
 document.querySelector('#addIngredient').onclick = () => {
 
     const ingredientInput = `<div class="input-group mb-3">
-        <input type="text" class="form-control" placeholder="Amount" name="amount">
-            <input type="text" class="form-control" placeholder="Name" name="name">
-            </div>`
+                            <input type="text" class="form-control" placeholder="Amount" name="amount">
+                            <input type="text" class="form-control" placeholder="Name" name="name">
+                        </div>`
 
 
     document.querySelector('.ingredient-inputs').innerHTML += ingredientInput
